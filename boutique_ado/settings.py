66285056f5e8265a94aff7ae01471b048baa267c
noreen <noreennelligancilms@gmail.com>
2023-03-04 23:11:32 +0000

    Change to settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -9,10 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
-from pathlib import Path
-
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%iv10d%mjd)&6elzslp_$1dk45^h2(xlr=4khfi5p#ts&ml6^0'
+SECRET_KEY = os.environ.get('SECRET_KEY')'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
